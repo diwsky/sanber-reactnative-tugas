e130f8d7bf9efd861c9efb3042a83cde8fdbd043
Diwangkoro <diwangkoro270@gmail.com>
2021-02-17 01:22:15 +0700

    update Tugas 2: adding soal no. 4

--- Tugas-2-String & Conditional/string.js
+++ Tugas-2-String & Conditional/string.js
@@ -35,8 +35,8 @@ console.log('Eighth Word: ' + eighthWord)
 // Soal no. 3 Mengurai Kalimat (Substring)
 var sentence2 = 'wow JavaScript is so cool';
 
-var exampleFirstWord2 = sentence2.substring(0, 3);
-var secondWord2 = sentence2.substring(4, 14);
+var exampleFirstWord2 = sentence2.substring(0, 3)
+var secondWord2 = sentence2.substring(4, 14)
 var thirdWord2 = sentence2.substring(15, 17)
 var fourthWord2 = sentence2.substring(18, 20)
 var fifthWord2 = sentence2.substring(21, 25)
@@ -48,3 +48,23 @@ console.log('Fourth Word: ' + fourthWord2);
 console.log('Fifth Word: ' + fifthWord2);
 
 // Soal no. 4
+var sentence3 = 'wow JavaScript is so cool';
+
+var exampleFirstWord3 = sentence3.substring(0, 3);
+var secondWord3 = sentence3.substring(4, 14)
+var thirdWord3 = sentence3.substring(15, 17)
+var fourthWord3 = sentence3.substring(18, 20)
+var fifthWord3 = sentence3.substring(21, 25)
+
+var firstWordLength = exampleFirstWord3.length
+var secondWordLength = secondWord3.length
+var thirdWordLength = thirdWord3.length
+var fourthWordLength = fourthWord3.length
+var fifthWordLength = fifthWord3.length
+
+// lanjutkan buat variable lagi di bawah ini 
+console.log('First Word: ' + exampleFirstWord3 + ', with length: ' + firstWordLength);
+console.log('Second Word: ' + secondWord3 + ', with length: ' + secondWordLength);
+console.log('Third Word: ' + thirdWord3 + ', with length: ' + thirdWordLength);
+console.log('Fourth Word: ' + fourthWord3 + ', with length: ' + fourthWordLength);
+console.log('Fifth Word: ' + fifthWord3 + ', with length: ' + fifthWordLength); 
