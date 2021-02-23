57549bdab67048ec2b7012ac6849322f52ba65ca
Diwangkoro Diwangkoro <diwangkoro270@gmail.com>
2021-02-23 22:54:08 +0700

    Tugas 7: Update jawaban

--- Tugas-7-ES6/es6.js
+++ Tugas-7-ES6/es6.js
@@ -1,26 +1,34 @@
 // nomor 1
 const golden = () => console.log("this is golden!!")
+golden()
 
 // nomor 2
-const newFunction = (firstName,fullName) => {
-    return {firstName,fullName,a = ()=>console.log(firstName+" "+fullName)}
+const newFunction = (firstName, lastName) => {
+	return {
+		firstName,
+		lastName,
+		fullName: () => console.log(firstName + " " + lastName),
+	}
 }
+newFunction("William", "Imoh").fullName()
 
 // nomor 3
 const newObject = {
-    firstName: "Harry",
-    lastName: "Potter Holt",
-    destination: "Hogwarts React Conf",
-    occupation: "Deve-wizard Avocado",
-    spell: "Vimulus Renderus!!!"
-  }
+	firstName: "Harry",
+	lastName: "Potter Holt",
+	destination: "Hogwarts React Conf",
+	occupation: "Deve-wizard Avocado",
+	spell: "Vimulus Renderus!!!",
+}
 
-const {firstName, lastName, destination, occupation} = newObject
+const { firstName, lastName, destination, occupation } = newObject
+console.log(firstName, lastName, destination, occupation)
 
 // nomor 4
 const west = ["Will", "Chris", "Sam", "Holly"]
 const east = ["Gill", "Brian", "Noel", "Maggie"]
-const combined = [...west,...east]
+const combined = [...west, ...east]
+console.log(combined)
 
 // nomor 5
 const planet = "earth"
@@ -29,3 +37,5 @@ var before = `Lorem ${view} dolor sit amet,
     consectetur adipiscing elit, ${planet}  do eiusmod tempor
     incididunt ut labore et dolore magna aliqua. Ut enim
     ad minim veniam`
+
+console.log(before)
