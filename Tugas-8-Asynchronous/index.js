f6d18be798a96a4241e1b1c32f0f01c945de2c59
Diwangkoro <diwangkoro270@gmail.com>
2021-02-26 00:06:47 +0700

    Tugas 9: Class

--- Tugas-8-Asynchronous/index.js
+++ Tugas-8-Asynchronous/index.js
@@ -6,12 +6,22 @@ let books = [
     { name: 'Kalkulus', timeSpent: 4000 }
 ]
 
-let idx = 0;
+let indexBuku = 0;
+// Fungsi untuk memanggil fungsi readBooks supaya bisa rekursif
 const iqro = (waktu, bukuSekarang) => {
-    readBooks(waktu, bukuSekarang, (time) => {
-        idx++
-        idx < books.length ? iqro(time, books[idx]) : console.log(`Semua buku telah dilihat/dibaca!`)
+    // Panggil fungsi readBooks dari callback.js
+    readBooks(waktu, bukuSekarang, (waktuSisa) => {
+        // Setelah dapat hasil dari callback, naikkan index untuk pilih buku selanjutnya
+        indexBuku = indexBuku + 1;
+        // Cek, apabila buku berikutnya masih ada, panggil fungsi sendiri (rekursif),
+        // gunakan parameter waktu tersisa dari callback
+        if (indexBuku < books.length) {
+            iqro(waktuSisa, books[indexBuku])
+        } else {
+            // end
+            console.log(`Semua buku telah dilihat/dibaca!`)
+        }
     })
 }
 
-iqro(5000, books[idx])
+iqro(5000, books[indexBuku])
